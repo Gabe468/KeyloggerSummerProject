b50c766148d38c7ab6bd1fc195ccc58149e74f92
Gabe468 <gabemendoza27@GMAIL.COM>
2023-08-01 14:29:23 -0400

    Creating Menu

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
 import keyboard
-print("Welcome to Keylogger")
+class Logger:
+    print("Welcome to Keylogger Project")
+
+    def saveRecording(recorded):
+        recordName = input("Enter name of saved file")
+        if recordName == None:
+            print("Name for file cannot be empty")
+        elif len(recordName) > 20:
+            print("Name for file is too long")
+        else: 
+            f = open(recordName, "x")
+
+    menu = input("Press 1 to record Keys: \nPress 2 to see recorded Keys: \n")
+
+    def record():
+        recorded = keyboard.record(until='esc')
+
+    while True:
+        if  menu == "1":
+            print("Recording... \nPress Esc to stop recording")
+            record()
+        elif menu == "2":
+            pass
+        else:
+            print("Incorrect Number. Please choose a number from the selection.")
+            menu = input("Press 1 to record Keys: \nPress 2 to see recorded Keys: \n")
+
+    
