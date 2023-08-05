ea030978f4c5111626ea24833ddb867f57f0733f
Gabe <gabemendoza27@gmail.com>
2023-08-04 23:51:36 -0400

    Need to add key file replay

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pynput import keyboard
+import os
 class Logger:
 
     #take in key, saves it and then prints out key
@@ -23,23 +24,34 @@ class Logger:
             listener.join()
             print(keysave)
             Logger.createSave(keysave)
+
+    #list the files of keylogs in folder keys
+    def listKeys():
+        os.chdir("keys")
+        print(os.listdir())
     
     def createSave(keysave):
         #selection to save
-        sel = input("Would you like to save the current keys? Y/N\n")
         while True:
+            sel = input("Would you like to save the current keys? Y/N\n")
             if sel.upper() == "Y":
                 name = input("Enter name for the file: \n")
                 #saves file
                 try:
+                    os.chdir("keys")
                     save = open(name, "x")
                     save.write(str(keysave))
+                    save.close()
                     print("Saved file named \"" + name +"\"" )
                     break
-                except Exception:
-                    print(Exception)
+                #raises error if a file with the same name is created
+                except FileExistsError as err:
+                    print(str(err)[11:])
             if sel.upper() == "N":
                 break
+    
+    def playKeys(file):
+        pass
 
 
 print("Welcome to Keylogger Project")
@@ -57,7 +69,9 @@ while True:
         break
     #selection for record saving
     elif menu == "2":
-        pass
+        Logger.listKeys()
+        playKeys(file)
+        break
     else:
         print("Incorrect Number. Please choose a number from the selection.")
         menu = input("Press 1 to record Keys: \nPress 2 to see recorded Keys: \n")
